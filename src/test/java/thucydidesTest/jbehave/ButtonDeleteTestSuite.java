666187b62f0f85099edd560e2710ac7d14fe0818
KonstGit <kostyan.vk@gmail.com>
2015-01-04 19:00:01 +0200

    75 %

---
+++ src/test/java/thucydidesTest/jbehave/ButtonDeleteTestSuite.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thucydidesTest.jbehave;
+
+/**
+ *
+ * @author Konst
+ */
+public class ButtonDeleteTestSuite {
+    
+}
